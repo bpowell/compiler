6ecc61e8625d27dcf20935ebac7167c92494947b
Brandon Powell <bpowell29a@gmail.com>
2015-12-23 20:54:16 -0500

    gets line num and char location

--- main.c
+++ main.c
@@ -8,6 +8,10 @@ typedef struct file_info {
     char *line;
 } FileInfo;
 
+void display_fileinfo(FileInfo fi) {
+    printf("\nLine no: %i\tChar no: %i\n", fi.linenumber, fi.charlocation);
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Needs args!");
@@ -16,7 +20,31 @@ int main(int argc, char *argv[]) {
 
     FileInfo fileInfo;
     fileInfo.filename = argv[1];
+    fileInfo.linenumber = fileInfo.charlocation = 0;
+
     FILE *fp = fopen(fileInfo.filename, "r");
+    if (fp==NULL) {
+        printf("Cannot open file!\n");
+        return 2;
+    }
+
+    char c;
+    for(;;) {
+        c = fgetc(fp);
+        if (feof(fp)) {
+            break;
+        }
+
+        display_fileinfo(fileInfo);
+        printf("%c", c);
+
+        if (c=='\n') {
+            fileInfo.charlocation = 0;
+            fileInfo.linenumber++;
+        }
+
+        fileInfo.charlocation++;
+    }
 
     fclose(fp);
     return 0;
